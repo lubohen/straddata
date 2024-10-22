16b5e32093f60272ffb8ad94643962ee562ecbc8
lubohen <lbhenrique.infonegocios@gmail.com>
2024-10-22 02:00:02 -0300

    Updates

---
+++ precache-manifest.86b15ab97e4fe8e2010d12b66309b8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a5e0df90d443b936539213cfd4d204",
+    "url": "/straddata/index.html"
+  },
+  {
+    "revision": "ef854348750787f8362a",
+    "url": "/straddata/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e8968b54b83728ca3a06",
+    "url": "/straddata/static/js/2.77f2a182.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/straddata/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "ef854348750787f8362a",
+    "url": "/straddata/static/js/main.ec12acb7.chunk.js"
+  },
+  {
+    "revision": "1170ac378653cdba6eb8",
+    "url": "/straddata/static/js/runtime~main.327eceef.js"
+  }
+]);
